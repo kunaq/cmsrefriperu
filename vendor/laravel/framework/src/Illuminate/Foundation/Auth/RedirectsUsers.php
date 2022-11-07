f5d3d66b684b335e41ada5926e64394424c5600c
mirellyagv <mirellyagv@gmail.com>
2022-11-07 15:19:19 -0500

    uploading all project
    include all files

---
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RedirectsUsers.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+trait RedirectsUsers
+{
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath() //ES la URL de redireccion cuando el usuario se loguea
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
+    }
+}
